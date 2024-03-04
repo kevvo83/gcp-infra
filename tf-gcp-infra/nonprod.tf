f55fa3dc5094859e5858f88a0be4659ef886c924
Kevin Lawrence <klawrence@simplemachines.com.au>
2024-03-04 13:02:59 +1100

    Create environment and hub/spoke architecture

--- tf-gcp-infra/nonprod.tf
+++ tf-gcp-infra/nonprod.tf
@@ -5,27 +5,32 @@ resource "google_project" "project" {
   auto_create_network = var.project_auto_create_default_network
 }
 
-resource "google_compute_network" "vpcs" {
-  for_each                = var.vpcs_list
-  name                    = each.value.name
-  auto_create_subnetworks = each.value.auto_create_subnetworks
-  routing_mode            = each.value.routing_mode
+resource "google_compute_network" hub-vpc {
+  name                    = "hub-vpc"
+  auto_create_subnetworks = false
+  routing_mode            = "GLOBAL"
+  project                 = google_project.project.project_id
+}
+
+resource "google_compute_network" spoke1-vpc {
+  name                    = "spoke1-vpc"
+  auto_create_subnetworks = false
+  routing_mode            = "GLOBAL"
   project                 = google_project.project.project_id
 }
 
 resource "google_compute_network_peering" "peering-hub-to-spoke1" {
   name         = "peering1"
-  network      = google_compute_network.vpcs[0].id
-  peer_network = google_compute_network.vpcs[1].id
+  network      = google_compute_network.hub-vpc.id
+  peer_network = google_compute_network.spoke1-vpc.id
 
   export_custom_routes = true
   import_custom_routes = true
 }
-
 resource "google_compute_network_peering" "peering-spoke1-to-hub" {
   name         = "peering2"
-  network      = google_compute_network.vpcs[1].id
-  peer_network = google_compute_network.vpcs[0].id
+  network      = google_compute_network.spoke1-vpc.id
+  peer_network = google_compute_network.hub-vpc.id
 
   export_custom_routes = true
   import_custom_routes = true
